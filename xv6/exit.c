f235def1752a5cc227ebf06960b47133e3a2430f
Ankur Boruah <aboruah@hawk.iit.edu>
2018-01-17 10:36:43 -0600

    Added exit.

---
+++ xv6/exit.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  printf(1, "%s", "I cannot exit. Press ctrl+a and then x to quit xv6.");
+  exit();
+}
